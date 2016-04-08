2e39f529a57099247b7d1340466ef0ec47191a99
ashleyraymaceli <ashley.maceli@gmail.com>
2016-04-08 15:11:06 -0700

    Add prototype to remove forms after user submits order

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,9 @@
 // Business Logic
-function AllOrders () {
-  this.pizzaTotal = [];
-}
-
 function Pizza (toppings, size, chosen) {
   this.pizzaToppings = toppings;
   this.pizzaSize = size;
   this.chosenToppings = chosen;
-  // this.pizzaTotal = [];
+  this.pizzaTotal = [];
 }
 
 Pizza.prototype.costOfToppings = function (chosen, toppings) {
@@ -25,7 +21,7 @@ Pizza.prototype.costOfPizza = function (toppings, size) {
 
 function resetFields() {
   $("select.new-pizza-size").val("");
-  $("input.checkbox").val();
+  $('input:checkbox').removeAttr('checked');
 }
 
 // User Interface Logic
@@ -33,24 +29,26 @@ $(document).ready(function(){
 
   $("#add-more-pizza").click(function(){
     $("#extra-pizzas").append('<p>___________________________________</p>' +
+                              '<h2>Sizes</h2>' +
                               '<div class="another-pizza">' +
                               '<select class="form-control new-pizza-size">' +
-                               '<option id="size1" value="10">Small</option>' +
-                               '<option id="size2" value="15">Medium</option>' +
-                               '<option id="size3" value="20">Large</option>' +
+                               '<option id="size1" value="10">Small - $10</option>' +
+                               '<option id="size2" value="15">Medium - $15</option>' +
+                               '<option id="size3" value="20">Large - $20</option>' +
                                '</select>' +
-                               '<h2>Toppings:</h2>' +
+                               '<h2>Toppings</h2>' +
+                               '<h3>$1 extra for each topping</h3>' +
                                '<div class="checkbox">' +
-                               '<label><input type="checkbox" name="toppings" value="1">Topping 1</label>' +
+                               '<label><input type="checkbox" name="toppings" value="1">Extra Cheese</label>' +
                                '</div>' +
                                '<div class="checkbox">' +
-                               '<label><input type="checkbox" name="toppings" value="1">Topping 2</label>' +
+                               '<label><input type="checkbox" name="toppings" value="1">Mushrooms</label>' +
                                '</div>' +
                                '<div class="checkbox">' +
-                               '<label><input type="checkbox" name="toppings" value="1">Topping 3</label>' +
+                               '<label><input type="checkbox" name="toppings" value="1">Peppers</label>' +
                                '</div>' +
                                '<div class="checkbox">' +
-                               '<label><input type="checkbox" name="toppings" value="1">Topping 4</label>' +
+                               '<label><input type="checkbox" name="toppings" value="1">Sausage</label>' +
                                '</div>' +
                                '</div>'
     );
@@ -65,18 +63,16 @@ $(document).ready(function(){
       var checkedBoxes = $(this).find( document.getElementsByName("toppings") );
 
       var newPizza = new Pizza(inputtedPizzaToppings, inputtedPizzaSize, checkedBoxes);
-      var newAllOrders = new AllOrders();
 
-      newAllOrders.pizzaTotal.push(newPizza)
+      newPizza.pizzaTotal.push(newPizza)
 
       newPizza.costOfToppings()
 
-      $("h1#pizza-price").append("$" + newPizza.costOfPizza());
+      $("#pizza-price").append("<li>" + "$" + newPizza.costOfPizza() + "</li>");
       $("#show-pizza-results").show();
-      console.log(newAllOrders.pizzaTotal);
     });
 
-    // resetFields()
+    resetFields();
 
   });
 });
